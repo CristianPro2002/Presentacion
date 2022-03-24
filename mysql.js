030ba6ae321b41ce2d0d1dbf77272bed8b57771d
CristianPro2002 <90258882+CristianPro2002@users.noreply.github.com>
2022-03-23 21:40:48 -0500

    Add files via upload

---
+++ mysql.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql')
+
+const conection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database:'banca',
+})
+
+conection.connect( (err)=>{
+    if(err) throw err
+    console.log('La conexion funciona')
+})
+
+const insertar =
+"INSERT INTO usuarios (IdUsuario,usuario,contraseña) VALUES ('2', 'Jose', 'Jose123')";
+conection.query(insertar, (err, rows) =>{
+    if(err) throw err;
+});
+
+conection.query("SELECT * from usuarios", (err, rows)=>{
+    if(err) throw err;
+    console.log("Los datos de la tabla son: ");
+    console.log(rows);
+    console.log("La cantidad de resultado es: ");
+    console.log(rows.lenght);
+});
+
+
+conection.end();
+
