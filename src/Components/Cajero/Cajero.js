cb862ac2de213787be9c338063a97787a0732184
CristianPro2002 <cristian.0713.cs@gmail.com>
2022-05-23 03:05:11 -0500

    calleseSeñora

--- src/Components/Cajero/Cajero.js
+++ src/Components/Cajero/Cajero.js
@@ -112,7 +112,7 @@ export const Cajero = (props) => {
           <div className="Container-2">
             <div className="container2">
               <Container>
-                <Form action="http://localhost/Recibo/Recibo.php" method="post">
+                <Form action="http://localhost:8080/Recibo/Recibo.php" method="post">
                   <div className="Contenedores2">
                     <div className="Contenedor-logo-second">
                       <img
